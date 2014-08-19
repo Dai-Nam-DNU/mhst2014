a646f2fa9b41c2ce79bb4dcc12ff4c0dab68d91e
DNU <dnusangtaomuahexanh@gmail.com>
2014-08-19 18:24:39 +0700

    chỉnh sửa lại tính năng search và pudlish release dành cho android opac 1.0

--- test1-master/test1-master/AndroidOpac/src/edu/dnu/androidopac/MainActivity.java
+++ test1-master/test1-master/AndroidOpac/src/edu/dnu/androidopac/MainActivity.java
@@ -1,33 +1,70 @@
 package edu.dnu.androidopac;
 
+import edu.dnu.androidopac.MainActivity;
+import edu.dnu.androidopac.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import edu.dnu.androidopac.log.LogConfig;
+import edu.dnu.androidopac.search.SearchFormActivity;
 
 public class MainActivity extends Activity {
-	protected void onCreate(Bundle savedInstanceState) {
+	static final String TAG = LogConfig.getLogTag(MainActivity.class);
+	// whether DEBUG level logging is enabled (whether globally, or explicitly
+	// for this log tag)
+	static final boolean DEBUG = LogConfig.isDebug(TAG);
+	// whether VERBOSE level logging is enabled
+	static final boolean VERBOSE = LogConfig.VERBOSE;
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button btnOpen=(Button)
-				findViewById(R.id.btnOpenChildActivity);
-		btnOpen.setOnClickListener(new 
-				View.OnClickListener() {
-			public void onClick(View arg0) {
-				doOpenChildActivity(); 
+		
+		Button btnSearch=(Button)findViewById(R.id.btnSearch);
+		btnSearch.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View arg0) 
+			{
+				Intent myIntent=new Intent(MainActivity.this, SearchFormActivity.class);
+				startActivity(myIntent);
 			}
 		});
-	}
-	public void doOpenChildActivity()
-	{
-		Intent myIntent=new Intent(this, ChildActivity.class);
-		startActivity(myIntent);
+		Button btnCredits =(Button) findViewById(R.id.btnCredits);
+		btnCredits.setOnClickListener(new OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Credits.class);
+				MainActivity.this.startActivity(intent);
+			}
+		})   ; 
 	}
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		boolean result = super.onCreateOptionsMenu(menu);
+		menu.add(Menu.NONE, Constants.PREFERENCES, 1, R.string.menu_preferences).setIcon(android.R.drawable.ic_menu_preferences);
+		//getMenuInflater().inflate(R.menu.activity_main, menu);
+		return result;		
+	}
+	public boolean onSearchRequested() {
+		startActivity(new Intent(this, SearchFormActivity.class));
 		return true;
 	}
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case Constants.SEARCH:
+			startActivity(new Intent(this, SearchFormActivity.class));
+			break;
+		case Constants.PREFERENCES:
+			startActivity(new Intent(this, EditPreferences.class));
+			break;		
+		}
+		return true;
+	}
+	
 }
